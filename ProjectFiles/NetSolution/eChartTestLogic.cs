ca7af68fdbed7337ef81252de232250176d10a0b
chinmay13tailor <chinmay.tailor@pima.in>
2024-05-09 10:32:35 +0530

    From Chinmay 1

--- ProjectFiles/NetSolution/eChartTestLogic.cs
+++ ProjectFiles/NetSolution/eChartTestLogic.cs
@@ -1,4 +1,3 @@
-#region Using directives
 using System;
 using UAManagedCore;
 using OpcUa = UAManagedCore.OpcUa;
@@ -16,7 +15,6 @@ using System.Timers;
 using FTOptix.EventLogger;
 using FTOptix.Alarm;
 using FTOptix.MicroController;
-#endregion
 
 public class eChartTestLogic : BaseNetLogic
 {
@@ -24,7 +22,8 @@ public class eChartTestLogic : BaseNetLogic
     public override void Start()
     {
         // Insert code to be executed when the user-defined logic is started
-        refreshTimer = new System.Timers.Timer(100000000); // 5000 milliseconds = 10 seconds
+        refreshTimer = new System.Timers.Timer(10000000); // 5000 milliseconds = 10 seconds
+
         refreshTimer.Elapsed += RefreshTimerElapsed; // Hook up the elapsed event
         refreshTimer.AutoReset = true; // Set the timer to repeat
         refreshTimer.Start();
@@ -32,23 +31,41 @@ public class eChartTestLogic : BaseNetLogic
 
     }
 
+
     public override void Stop()
     {
         // Insert code to be executed when the user-defined logic is stopped
-        refreshTimer.Stop();
+        refreshTimer?.Stop();
+    }
+
+    // Method to handle refresh button click
+    [ExportMethod]
+    public void OnRefreshButtonClicked()
+    {
+        // Stop the timer if running
+        Thread.Sleep(500);
+        refreshTimer?.Stop();
+
+        /* Start the timer for refresh
+        refreshTimer.Interval = 1000; // 10 seconds
+        refreshTimer.Elapsed += RefreshTimerElapsed; // Hook up the elapsed event
+        refreshTimer.AutoReset = true; // Set the timer to repeat
+        refreshTimer.Start();
+        
+        */ // Call RefreshRadarGraph immediately when button is clicked
+        RefreshRadarGraph();
     }
 
     private void RefreshTimerElapsed(object sender, ElapsedEventArgs e)
     {
-        // This method is called every 5 seconds
+        // This method is called every 10 seconds by timer
         RefreshRadarGraph();
     }
 
-    [ExportMethod]
-    public void RefreshRadarGraph()
+    // Method to refresh radar graph
+    private void RefreshRadarGraph()
     {
-        Owner.Get<WebBrowser>
-        ("WebBrowser").Visible = false;
+        Owner.Get<WebBrowser>("WebBrowser").Visible = false;
         Log.Debug("eCharts", "Starting");
         String projectPath = (ResourceUri.FromProjectRelativePath("").Uri);
         String folderSeparator = Path.DirectorySeparatorChar.ToString();
@@ -62,28 +79,17 @@ public class eChartTestLogic : BaseNetLogic
 
         // Insert values
         for (int i = 1; i < 32; i++)
-
-       //     Int32 value = 0;
-       // if (Project.Current.GetVariable("Model/eCharts/eChart1" + i).Value != null)
-       // {
-       //    value = Project.Current.GetVariable("Model/eCharts/eChart1" + i).Value * 1;
-       //  }
-       // text = text.Replace(i < 10 ? "$0" + i : "$" + i, value.ToString());
-
-         {
-           text = text.Replace(i < 10 ? "$0" + i : "$" + i, (Project.Current.GetVariable("Model/eCharts/Day" + i).Value * 1).ToString());
-         }
+        {
+            text = text.Replace(i < 10 ? "$0" + i : "$" + i, (Project.Current.GetVariable("Model/eCharts/Day" + i).Value * 1).ToString());
+        }
 
         // Write to file
         File.WriteAllText(filePath, text);
 
         // Refresh WebBrowser page
-        Owner.Get<WebBrowser>
-            ("WebBrowser").Refresh();
+        Owner.Get<WebBrowser>("WebBrowser").Refresh();
         Log.Debug("eCharts", "Finished");
         Thread.Sleep(500);
-       // ref.data(500);
-        Owner.Get<WebBrowser>
-            ("WebBrowser").Visible = true;
+        Owner.Get<WebBrowser>("WebBrowser").Visible = true;
     }
 }
